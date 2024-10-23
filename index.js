59e2bd1962977cdb1ccc702f3437b09e4738d300
Shresht1706 <156106644+Shresht1706@users.noreply.github.com>
2024-10-23 17:00:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -216,8 +216,3 @@ client.on("messageCreate", async (message) => {
     await sendDailyEvents(message.channel);
   }
 });
-
-const express = require("express");
-const app = express(); 
-app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
-const PORT = process.env.PORT || 5000; app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`); });
